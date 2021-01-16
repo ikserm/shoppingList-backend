c53456a999e5bd688a59f4d60c64653edb1cba14
ikserm <iksermd@gmail.com>
2021-01-16 00:15:39 -0600

    Create insert, post

--- src/articles.js
+++ src/articles.js
@@ -4,6 +4,20 @@ function createRouter(db) {
     const router = express.Router();
     const user = '';
 
+    router.post('/article', (req, res, next) => {
+        db.query(
+            'INSERT INTO articles (user, name, description, estate, date) VALUES (?,?,?,?)', [user, req.body.name, req.body.description, req.body.estate, new Date(req.body.date)],
+            (error) => {
+                if (error) {
+                    console.error(error);
+                    res.status(500).json({ status: 'error' });
+                } else {
+                    res.status(200).json({ status: 'ok' });
+                }
+            }
+        );
+    });
+
     return router;
 }
 
