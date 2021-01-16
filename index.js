e076216e506e32bb5c6c319cd4c97acf03bf458a
ikserm <iksermd@gmail.com>
2021-01-15 23:32:52 -0600

    Create index.js
    Add database conection and express instance

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const events = require('./events');
+
+const connect = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'shoppinglist'
+});
+
+connect.connect();
+
+const port = process.env.PORT || 8080;
+
+const app = express()
+    .use(cors())
+    .use(bodyParser.json())
+    .use(events(connect));
+
+app.listen(port, () => {
+    console.log('Server stared, port: ' + port);
+});
